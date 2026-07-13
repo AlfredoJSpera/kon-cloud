b8c188b828eadc01600604dd1c9bee47b0c0d815
AlfredoJSpera <alfredo.j.spera@gmail.com>
2026-07-13 15:34:54 +0200

    refactor(backend): Rename .env PORT and add JWT secret variable in example

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -3,7 +3,7 @@ import pino from "pino";
 import { pinoHttp } from "pino-http";
 
 export const logger = pino({
-	level: process.env.LOG_LEVEL || "info",
+	level: process.env.SV_LOG_LEVEL || "info",
 	transport: {
 		target: "pino-pretty",
 		options: {
