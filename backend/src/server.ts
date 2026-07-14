0ab807ba24cd2a76f2395024afcd959b680ee756
AlfredoJSpera <alfredo.j.spera@gmail.com>
2026-07-14 14:13:34 +0200

    refactor(backend): Rename authentication and administrator file routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import authRoutes from "@routes/authRoutes";
-import administratorsRoutes from "@routes/administratorsRoutes";
+import authRoutes from "@routes/auth";
+import administratorsRoutes from "@routes/administrator";
 import { prismaErrorHandler } from "@middleware/errorHandler";
 import { loggerHttp, logger } from "@middleware/logger";
 import { rateLimit } from "express-rate-limit";
