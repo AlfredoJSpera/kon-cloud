5116ad0a6575ec6c78472a4c55f3ec529d089432
AlfredoJSpera <alfredo.j.spera@gmail.com>
2026-07-21 14:04:18 +0200

    feat(frontend): Implement login functionality with error handling and API integration

---
+++ frontend/src/api/apiErrorHandler.ts
@@ -0,0 +1,19 @@
+const ErrorMessages: Record<string, string> = {
+	// Validation
+	MISSING_REQUIRED_FIELDS: "You missed some required information.",
+	INCORRECT_FIELD_TYPE: "Please check the format of your input.",
+	NOT_FOUND: "The requested information could not be found.",
+	EMAIL_ALREADY_EXISTS: "That email is already in use. Try another.",
+	// Authentication
+	INVALID_CREDENTIALS: "The email or password you entered is incorrect.",
+	MISSING_REFRESH_TOKEN: "Your session has ended. Please log in again.",
+	INVALID_REFRESH_TOKEN: "Session invalid. Please log in again.",
+	MISSING_AUTHENTICATION_TOKEN: "You must be logged in to access this page.",
+	INVALID_AUTHENTICATION_TOKEN: "Authentication failed. Please log in again.",
+	EXPIRED_AUTHENTICATION_TOKEN:
+		"Your session has expired. Please log in again.",
+};
+
+export default function handleApiError(errorCode: string) {
+	return ErrorMessages[errorCode] || "Something went wrong.";
+}
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,14 +1,93 @@
 import { Button, Input, Link, Separator, Stack, Text } from "@chakra-ui/react";
 import { useState } from "react";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { LuArrowRight } from "react-icons/lu";
 import { Field } from "@/components/ui/field";
 import { PasswordInput } from "@/components/ui/password-input";
 import { AuthContainer } from "@/components/AuthContainer";
+import { loginUrl } from "@/api/apiUrls";
+import { type IErrorResponse } from "@backend-interfaces/common";
+import {
+	type IAuthLoginInput,
+	type IAuthLoginOutput,
+} from "@backend-interfaces/auth";
+import { toaster } from "@/components/ui/toaster";
+import handleApiError from "@/api/apiErrorHandler";
 
 export function LoginPage() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const [fieldErrors, setFieldErrors] = useState({
+		email: false,
+		password: false,
+	});
+	const navigate = useNavigate();
+
+	const handleLogin = async (e: React.SubmitEvent<HTMLDivElement>) => {
+		e.preventDefault();
+		setFieldErrors({ email: false, password: false });
+		setIsLoading(true);
+
+		try {
+			if (!email || !password) {
+				// Set errors if fields are empty
+				setFieldErrors({ email: !email, password: !password });
+				throw new Error("MISSING_REQUIRED_FIELDS");
+			}
+
+			const input: IAuthLoginInput = {
+				email,
+				password,
+			};
+
+			const response = await fetch(loginUrl, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(input),
+			});
+
+			const data = await response.json();
+
+			if (!response.ok) {
+				const errorData = data as IErrorResponse;
+				throw new Error(errorData.errorCode);
+			}
+
+			const responseData = data as IAuthLoginOutput;
+
+			//TODO: Change localStorage for a React Context
+			localStorage.setItem("accessToken", responseData.accessToken);
+			localStorage.setItem("csrfToken", responseData.csrfToken);
+			localStorage.setItem(
+				"userProfile",
+				JSON.stringify(responseData.profile),
+			);
+
+			toaster.create({
+				title: "You are now logged in.",
+				type: "success",
+			});
+
+			navigate("/dashboard");
+		} catch (err: unknown) {
+			console.error("Login error:", err);
+
+			const errorMessage =
+				err instanceof Error
+					? handleApiError(err.message)
+					: "Something went wrong.";
+
+			toaster.create({
+				title: errorMessage,
+				type: "error",
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<AuthContainer
@@ -17,38 +96,54 @@ export function LoginPage() {
 			actionLabel="Sign in"
 			actionTitle="Login to Kon-Cloud"
 		>
-			<Stack
-				gap="4"
-				as="form"
-				onSubmit={(e) => {
-					e.preventDefault();
-					console.log("login:", { email, password });
-				}}
-			>
-				{/* Login info */}
-
-				<Field label="Email address">
+			<Stack gap="4" as="form" onSubmit={handleLogin}>
+				<Field
+					label="Email address"
+					invalid={fieldErrors.email}
+					errorText="This field is required"
+				>
 					<Input
 						placeholder="name@company.com"
 						value={email}
-						onChange={(e) => setEmail(e.target.value)}
+						onChange={(e) => {
+							setEmail(e.target.value);
+							if (e.target.value)
+								setFieldErrors((prev) => ({
+									...prev,
+									email: false,
+								}));
+						}}
+						disabled={isLoading}
 					/>
 				</Field>
-				<Field label="Password">
+				<Field
+					label="Password"
+					invalid={fieldErrors.password}
+					errorText="This field is required"
+				>
 					<PasswordInput
 						placeholder="Enter your password"
 						value={password}
-						onChange={(e) => setPassword(e.target.value)}
+						onChange={(e) => {
+							setPassword(e.target.value);
+							if (e.target.value)
+								setFieldErrors((prev) => ({
+									...prev,
+									password: false,
+								}));
+						}}
+						disabled={isLoading}
 					/>
 				</Field>
 
-				{/* Submit */}
-
-				<Button type="submit" size="lg">
+				<Button
+					type="submit"
+					size="lg"
+					loading={isLoading}
+					disabled={isLoading}
+				>
 					Login
 				</Button>
-
-				{/* Registration link */}
 
 				<Text fontSize="sm" textAlign="center">
 					<Link asChild>
@@ -57,8 +152,6 @@ export function LoginPage() {
 						</RouterLink>
 					</Link>
 				</Text>
-
-				{/* OAuth links */}
 
 				<Separator />
 				<Stack gap="3">
